5aa4022bd8678258a85787736b4e7691fbbda58d
Voldemort-1 <piyushd12121@gmail.com>
2021-03-01 12:34:37 +0530

    Added Start Script

--- app.js
+++ app.js
@@ -252,6 +252,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode);
   res.render('error', { err });
 })
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+const port=process.env.PORT||3000;
+app.listen(port, function () {
+  console.log("Server started on port: " + port);
 });
